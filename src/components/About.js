b5fa1b9454e472530a8c9c4405fd3799f9affe28
vmangahis <just.monika092299@gmail.com>
2023-01-15 00:41:13 +0800

    updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -12,15 +12,15 @@ const About = () => {
       <div className="about-info">
         <img src="https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/portfolio-photo.JPG?alt=media&token=f72d9c89-b609-430f-a927-fb1dce1f0743" className="about-photo" alt="My portfolio" />
         
+        <div className="about-info-text">
+          <p>
+            Hi, my name is Vince Mangahis. I am a web developer from Santa Rosa, Laguna. Solving problems through creation of programs or web applications are one of my hobbies.
+          </p>
 
-        <ul className="about-info-text">
-          <li>Name: Vince Baron Mangahis</li>
-          <li>Address: Santa Rosa, Laguna</li>
-          <li>Gender: Male</li>
-          <li>Birthday: September 22, 1999</li>
-          <li>Contact Number: +639057555624</li>
-          <li>Contact Email: vince.baron092299@gmail.com</li>
-        </ul>
+          <p>As a programmer, I am eager to learn more skills in my field. Currently I am focusing on building software using Python along with the needed frameworks like Django</p>
+
+        </div>
+        
 
       </div>
       </div>
