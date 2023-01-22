16ddf759bd19f20d396c2514f2b62ebc79dcebf9
vmangahis <just.monika092299@gmail.com>
2023-01-22 23:32:53 +0800

    fixed repeating image

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,23 +9,27 @@ const Projects = () => {
 
     <div className="project-container">
 
-      <a href="https://github.com/vmangahis/apartment-rental-system" className='project-card' style={{ backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/apt.jpg?alt=media&token=3be48125-394f-48b1-8db1-bec6de962a21)'}} onMouseEnter={(e) => {
-        e.target.style.background = `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.9)), url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/apt.jpg?alt=media&token=3be48125-394f-48b1-8db1-bec6de962a21)`
+      <a href="https://github.com/vmangahis/apartment-rental-system" className='project-card' style={{ backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/apt.jpg?alt=media&token=3be48125-394f-48b1-8db1-bec6de962a21)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}} onMouseEnter={(e) => {
+        e.target.style.background = `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.9)), url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/apt.jpg?alt=media&token=3be48125-394f-48b1-8db1-bec6de962a21) no-repeat`
+        e.target.style.backgroundRepeat =  'no-repeat';
+        e.target.style.backgroundSize = 'cover';
       }}
       onMouseLeave={(e) =>{
-        e.target.style.background = `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0)), url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/apt.jpg?alt=media&token=3be48125-394f-48b1-8db1-bec6de962a21)`
+        e.target.style.background = `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0)), url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/apt.jpg?alt=media&token=3be48125-394f-48b1-8db1-bec6de962a21) no-repeat`
+        e.target.style.backgroundRepeat =  'no-repeat';
+        e.target.style.backgroundSize = 'cover';
       }}
       >
       <p className="project-name">Apartment Rental Management System</p>
       </a>
 
 
-      <a href="https://github.com/vmangahis/Kai" className="project-card" style={{ backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/kai.PNG?alt=media&token=c7f53b22-873c-44f8-8356-6d3fc37b3d0b)'}}
+      <a href="https://github.com/vmangahis/Kai" className="project-card" style={{ backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/kai.PNG?alt=media&token=c7f53b22-873c-44f8-8356-6d3fc37b3d0b) no-repeat', backgroundRepeat: 'no-repeat'}}
       onMouseEnter={(e) => {
-        e.target.style.background = `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.9)), url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/kai.PNG?alt=media&token=c7f53b22-873c-44f8-8356-6d3fc37b3d0b)`
+        e.target.style.background = `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.9)), url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/kai.PNG?alt=media&token=c7f53b22-873c-44f8-8356-6d3fc37b3d0b) no-repeat`
       }}
       onMouseLeave={(e) =>{
-        e.target.style.background = `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0)), url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/kai.PNG?alt=media&token=c7f53b22-873c-44f8-8356-6d3fc37b3d0b)`
+        e.target.style.background = `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0)), url(https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/kai.PNG?alt=media&token=c7f53b22-873c-44f8-8356-6d3fc37b3d0b) no-repeat`
       }}
       >
       <p className="project-name">Kai</p>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,19 +3,19 @@ import SkillBar from 'react-skillbars'
 
 const Skills = () => {
 
-  const skills = [
-    {type: 'Python', level: 90},
-    {type: 'Django', level: 85},
-    {type: 'PHP', level: 75},
-    {type: 'Laravel', level: 70},
-    {type: 'HTML', level: 70},
-    {type: 'Javascript', level: 70},
-    {type: 'JQuery', level: 70},
-    {type: 'React', level: 65},
-    {type: 'ExpressJS', level: 65},
-    {type: 'NodeJS', level: 60},
-    {type: 'CSS', level: 50},
-  ]
+  // const skills = [
+  //   {type: 'Python', level: 90},
+  //   {type: 'Django', level: 85},
+  //   {type: 'PHP', level: 75},
+  //   {type: 'Laravel', level: 70},
+  //   {type: 'HTML', level: 70},
+  //   {type: 'Javascript', level: 70},
+  //   {type: 'JQuery', level: 70},
+  //   {type: 'React', level: 65},
+  //   {type: 'ExpressJS', level: 65},
+  //   {type: 'NodeJS', level: 60},
+  //   {type: 'CSS', level: 50},
+  // ]
 
 
   const colors = {
@@ -30,8 +30,8 @@ const Skills = () => {
   return (
     <div className='skills' id="skills">
       <h1>Skills</h1>
-
-      <SkillBar skills = {skills} colors={colors} />
+      
+      {/* <SkillBar skills = {skills} colors={colors} /> */}
       </div>
   )
 }
