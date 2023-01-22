de3a5fce11cfcc0b93aec7bb2fe867ad7332b870
vmangahis <just.monika092299@gmail.com>
2023-01-22 23:19:23 +0800

    styling changes in the experience section

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,13 @@
-import React from 'react'
+
 
 
 const About = () => {
 
-
-
   return (
     <div className="about" id="about">
-      <h1>About me</h1>
+      <h1>About Me</h1>
 
-      <div className="about-info">
+      <section className="about-info">
         <img src="https://firebasestorage.googleapis.com/v0/b/vm-dev-fa2be.appspot.com/o/portfolio-photo.JPG?alt=media&token=f72d9c89-b609-430f-a927-fb1dce1f0743" className="about-photo" alt="My portfolio" />
         
         <div className="about-info-text">
@@ -17,14 +15,15 @@ const About = () => {
             Hi, my name is Vince Mangahis. I am a web developer from Santa Rosa, Laguna. Solving problems through creation of programs or web applications are one of my hobbies.
           </p>
           <br />
-          <p>As a programmer, I am eager to learn more skills in my field. Currently I am focusing on building software using Python along with the needed frameworks like Django.</p>
+          <p>As a programmer, I am eager to learn more skills in my field. Currently I am focusing on building software using Python 
+            along with the needed frameworks like Django.</p>
 
         </div>
         
 
-      </div>
+      </section>
       </div>
   )
 }
 
-export default About
+export default About;
