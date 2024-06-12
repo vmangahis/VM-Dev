2057fb4e7e510a4fc08b6c6c0b379724cb970bc8
vmangahis <just.monika092299@gmail.com>
2024-06-13 00:39:15 +0800

    changed contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,13 +9,13 @@ const Contact = () => {
       <form className="contact-form" method="POST" >
         
         <div className="form-control">
-        <label htmlFor="">Email:</label>
-        <input type="email"  className="email-input" placeholder="Email address..."/>
+        <label htmlFor="">Name:</label>
+        <input type="text"  className="email-input" placeholder="Your name..."/>
         </div>
 
         <div className="form-control">
-        <label htmlFor="email-body">Body:</label>
-        <textarea placeholder="Email body...." className="email-body">
+        <label htmlFor="email-body">Message:</label>
+        <textarea placeholder="Your message..." className="text-body">
         </textarea>
         </div>
 
