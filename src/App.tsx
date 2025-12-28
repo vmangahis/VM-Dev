7abe95d38c679fbbc94b927553579c3411900b5b
vmangahis <vince.baron092299@gmail.com>
2025-12-28 17:53:05 +0800

    removed contact form

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import About from './components/About'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
-import Contact from './components/Contact'
 import Footer from './components/Footer'
 import './App.css';
 
@@ -18,7 +17,6 @@ function App() {
         <Experience />
         <Projects />
         <Skills />
-        <Contact />
         <Footer />
     </div>
   );
